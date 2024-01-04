ecc278af2a118678b223cda6090931bfbf68073b
istekhar khan <istekharkhan46@gmail.com>
2024-01-05 01:09:11 +0530

    vite confige changed

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  server:{
+    port: 3000,
+    proxy:  {}
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
