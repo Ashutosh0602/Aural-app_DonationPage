5c85bee1ba3e6a30cc262d59e5d13027026e0712
Ashutosh Rai <ashu00jnp@gmail.com>
2024-01-05 10:37:26 +0530

    update firstHalf

--- src/screen/secondHalf/SecondHalf.tsx
+++ src/screen/secondHalf/SecondHalf.tsx
@@ -1,5 +1,14 @@
 function SecondHalf() {
-  return <div>SecondHalf</div>;
+  return (
+    <section>
+      <div>
+        <div className="text-center bg-red-500">We Love To Help Poor</div>
+        <div>Our Featured Campaigns</div>
+      </div>
+      <div></div>
+      <div></div>
+    </section>
+  );
 }
 
 export default SecondHalf;
